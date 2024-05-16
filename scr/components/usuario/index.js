c4bd7b0a40c0192b5c54d099f9a36f134b1d5a97
Aparecida-Lima19 <ci.lima.silva22@gmail.com>
2024-05-16 02:40:25 -0300

    Criação do degrade das telas...
    implementação do degrade e correção de códigos

--- scr/components/usuario/index.js
+++ scr/components/usuario/index.js
@@ -1,17 +1,21 @@
 import React  from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 import styles from "./style";
 
 export default function Usuário() {
     
 
     return (
-        <View style={styles.container}>
+        <LinearGradient
+        colors={['#00FFFF', '#1782AF']}
+        style={styles.container}
+        >
             <Text style={styles.label}>Usuário:</Text>
             <TextInput
                 style={[styles.input, { textAlign: 'center' }]} 
                 placeholder="ex: Lívia Maria"
             /> 
-        </View>
+        </LinearGradient>
     );
 }
--- scr/components/welcome/index.js
+++ scr/components/welcome/index.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
-
-import{ useNavigation} from '@react-navigation/native'
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Welcome() {
-
     // Definindo o estado para armazenar o tamanho da imagem
     const [size, setSize] = useState({ width: 100, height: 100 });
+
+    //Definindo a navegação de telas
+    const navigation = useNavigation();
 
     // Função para aumentar o tamanho da imagem
     const increaseSize = () => {
@@ -16,11 +18,11 @@ export default function Welcome() {
         }));
     };
 
-    //Definindo a navegação de telas
-    const navigation = useNavigation();
-
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={['#00FFFF', '#1782AF']}
+            style={styles.container}
+        >
             <View style={styles.containerTitle}>
                 <Text style={styles.title}>Enade</Text>
             </View>
@@ -31,71 +33,67 @@ export default function Welcome() {
                     style={[styles.image, { width: size.width, height: size.height }]}
                 />
 
-                <TouchableOpacity 
-                style={styles.button}
-                onPress={ () => navigation.navigate('Usuario')}
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.navigate('Usuario')}
                 >
                     <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
             </View>
-        </View>
+        </LinearGradient>
     );
 }
 
+//Função para definir o container
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
 
-    //Função para definir o container
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: '#00FFFF',
-        },
+    //Estilização para o container do titulo...
+    containerTitle: {
+        flex: 3,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
-        //Estilização para o container do titulo...
-        containerTitle: {
-            flex: 3,
+    //Estilização para o container do Logo...
+    containerLogo: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
+    //Estilização do text do titulo...
+    title: {
+        color: '#FFFFFF',
+        fontSize: 35,
+        fontWeight: 'bold'
+    },
 
-        //Estilização para o container do Logo...
-        containerLogo: {
-            flex: 1,  
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
+    //Estilização da logo...
+    image: {
+        resizeMode: 'contain',
+    },
 
-        //Estilização do text do titulo...
-        title: {
-            color: '#FFFFFF',
-            fontSize: 35,
-            
-            fontWeight: 'bold'
-        },
+    //Estilização do button de acesso...
+    button: {
+        position: 'absolute',
+        backgroundColor: '#007BFF',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '30%',
+        alignSelf: 'center',
+        bottom: '15%',
+        justifyContent: 'center',
+        alignItems: 'center'
 
-        //Estilização da logo...
-        image: {
-            resizeMode: 'contain',
-        },
+    },
 
-        //Estilização do button de acesso...
-        button: {
-            position: 'absolute',
-            backgroundColor: '#007BFF',
-            borderRadius: 50,
-            paddingVertical: 8,
-            width: '30%',
-            alignSelf: 'center',
-            bottom:'15%',
-            justifyContent: 'center',
-            alignItems: 'center'
-            
-        },
-        
-        //Estilização do text do button...
-        buttonText: {
-            color: '#FFFFFF',
-            fontSize: 16,
-            fontWeight: 'bold',
-        }
-    });
+    //Estilização do text do button...
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
+});
