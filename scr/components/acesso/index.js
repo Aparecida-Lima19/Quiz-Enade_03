4c14cc023ea843916aa83ca66863085c9dbcecea
Aparecida-Lima19 <ci.lima.silva22@gmail.com>
2024-05-19 09:53:32 -0300

    Terceira tela
    Iniciei a terceira tela, salvando para não perder...

--- scr/components/acesso/index.js
+++ scr/components/acesso/index.js
@@ -1,21 +1,25 @@
 import React from "react";
-import {View, Text, StyleSheet } from "react-native"
+import {View, Text, Pressable  } from "react-native"
 import { LinearGradient } from 'expo-linear-gradient';
+import styles from "./style";
 
 export default function Acesso(){
     return(
         <LinearGradient
         colors={['#00EFFF', '#00E0FF', '#174BAF']}
         style={styles.container}>
-            <View>
+            <View style={styles.containerTopo}>
                 <Text>Tela De Escolha de</Text>
+            </View>
+            <View style={styles.containerItens}>
+                <Text>Tela De Escolha de</Text>
+                <View style={styles.body}>
+                    <Pressable style={styles.playButton}>
+                        <Text style={styles.buttonText}>PLAY</Text>
+                    </Pressable>
+                </View>
             </View>
         </LinearGradient>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-})
---
+++ scr/components/acesso/style.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  containerTopo: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    //backgroundColor:'yellow',
+    width:'100%',
+  },
+  containerItens: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    //backgroundColor:'red',
+    width:'100%',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  playButton: {
+    backgroundColor: '#2089DC',
+    padding: 15,
+    borderRadius: 5,
+  },
+  });
+  export default styles;
